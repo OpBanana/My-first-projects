143663def8283cd5bfe4853b4aa9bb542245a2f5
Max <45742433+OpBanana@users.noreply.github.com>
2018-12-11 21:41:50 +1100

    Add files via upload

---
+++ Australian Animal Classifier.py
@@ -0,0 +1,61 @@
+# All animals must be lower-case, as python is case-sensitive
+# All names must be typed with an underscore instead of spaces, as Python does not recognise
+# Spaces within your program
+# To quit, you must use "quit ()"
+print ("All animals must be lower-case, as python is case-sensitive.")
+print ("All names must be typed with an underscore instead of spaces, as Python does not recognise Spaces within your program To quit, you must use quit ()")
+print ("You must also double click enter before entering an animal name")
+error = "Animal is either not defined, the program is not working, or you have misspelt your animal. Please check, and try again."
+na = "Please enter a new animal, if you would like to exit, use the command 'quit()'"
+eastern_gray_kangaroo = "Macropus giganteus"
+human = "Homo Sapiens"
+red_necked_wallaby = "Macropus rufogriseus"
+parrot = "Psittaciformes"
+emu = "Dromaius novaehollandiae"
+koala = "Phascolarctos cinereus"
+tasmanian_devil = "Sarcophilus harrisii"
+wombat = "Vombatidae"
+echidna = "Tachyglossidae"
+possum = "Phalangeriformes"
+brown_snake = "Pseudonaja"
+kookaburra = "Dacelo"
+platypus = "Ornithorhynchus anatinus"
+what_animal = input("What is the animal?")
+test = "testineed"
+Sn = "Scientific name:"
+animal = input()
+if animal == "quit":
+    exit(quit)
+if animal == human:
+  print (Sn, human, na)
+if animal == eastern_gray_kangaroo:
+  print (Sn, eastern_gray_kangaroo, na)
+if animal == red_necked_wallaby:
+  print (Sn, red_necked_wallaby, na)
+if animal == parrot:
+  print (Sn, parrot, na)
+if animal == emu:
+    print(Sn, emu, na)
+if animal == koala:
+    print(Sn, koala, na)
+if animal == tasmanian_devil:
+    print (Sn, tasmanian_devil, na)
+if animal == wombat:
+    print (Sn,wombat,na) 
+if animal == echidna:
+    print (Sn, echidna, na)
+if animal == platypus:
+    print (Sn, platypus, na)
+if animal == possum:
+    print (Sn, possum, na)
+if animal == kookaburra:
+    print (Sn, kookaburra, na)
+if animal == brown_snake:
+    print (Sn, brown_snake, na)
+
+
+
+    
+
+
+
---
+++ Overwatch_hero_dictionary_test.py
@@ -0,0 +1,43 @@
+# To see the description of a specific hero, use the command below
+# print (hero name) then click "run" in the top task bar, then click "Run Module"
+# When you have done this, a window should show with the output, the output being
+# a brief, memey description of the hero you chose.
+# soldier dows not have numbers, just "soldier"
+# Wrecking ball is "wrecking_ball" with an underscore
+# To use this program, you will need to open it with python, below are the download links.
+# Windows: https://www.python.org/ftp/python/3.7.1/python-3.7.1-amd64.exe
+# macOS: https://www.python.org/ftp/python/3.7.1/python-3.7.1-macosx10.6.pkg
+mccree = "Mccree: pistol boi with aimbot ult"
+orisa = "Orisa: most cancer 1v1 ever"
+soldier = "Soldier: literally cod in overwatch, with an aimbot ult"
+tracer = "Tracer: always cancer, always, besties with another cancer hero, Winston."
+bastion = "Bastion: campy tracer"
+ashe = "Ashe: always chosen first, for now at least"
+dva = "Dva: least defensive tank, basically a PHAT dps"
+zarya = "Zarya: russian lesbian"
+wrecking_ball = "Wrecking_ball: SKRT SKRT PULL UP IN THE HOOD"
+winston = "Winston: cancer, tracers bestie, which is also a cancereous hero"
+junkrat = "Junkrat: Australian maniac, besties with Roadhog, also Australian"
+roadhog = "Roadhog: THICC Australian, besties with junkrat"
+hanzo = "Hanzo: little kids that think they're cool"
+genji = "Genji: hanzos lil brother, lil less cancer"
+reinhardt = "Reinhardt: big shield"
+doomfist = "Doomfist: big punch"
+zenyatta = "Zenyatta: floating omnic that shoots orbs at allies and enemies."
+moira = "Moira: a support version of winston"
+torbjorn = "Torbjorn: swedish mechanic dwarf, turned fighter for good"
+mei = "Mei: freezes you then shoots an icicle through your head while smiling"
+mercy = "Mercy: long rod that heals and damage boosts allies, and a small pistol to defend herself"
+symmetra = "Symmetra: pretty much torbjorn but with more turrets that do less damage"
+widowmaker = "Widowmaker: sniper with a poison mine and wall hack ult"
+pharah = "Pharah: DEATH RAINS FROM ABOVE"
+reaper = "Reaper:shotgun kiddo that goes behind you then pops your head"
+ana = "Ana:support version of widowmaker, with an ult that boosts an allies health and damage output"
+brigitte = "Brigitte: reinhardts friend, with a mace she whacks people with, and a small shield, she has packs that heal allies and give armor"
+sombra = "Sombra: unrealistic hacker that can teleport and go invisible."
+lucio = "Lucio: very good hero to start as, heals and boosts speed, ult gives everyone shield going down"
+# type commands under this
+# type commands under this
+# type commands under this
+# type commands under this
+
